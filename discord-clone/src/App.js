845ba2bcb998052885ecbc9592753770bdc58232
michellewong27 <michellekwong2@gmail.com>
2021-08-10 12:53:52 -0400

    Dark Mode

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -6,12 +6,16 @@ function App() {
 
   const YOUR_APP_ID = '87FFAEDA-9FEF-429D-9C56-F12EC1234049';
   const USER_ID = '1';
-
+  const theme = 'dark';
+  const allow_edit_profile = true;
+  
   return (
     <div className="App">
          <SendBirdApp
                 appId={YOUR_APP_ID}    
                 userId={USER_ID}
+                theme={theme}
+                allowProfileEdit={allow_edit_profile}       
             />
     </div>
   );
