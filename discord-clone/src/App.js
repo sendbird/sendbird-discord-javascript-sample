2d25e7a95de7318d2fcef5ce1312b611fe42e54d
michellewong27 <michellekwong2@gmail.com>
2021-08-10 20:28:49 -0400

    Gray color scheme with colorSet

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -8,23 +8,40 @@ function App() {
 
   const APP_ID = '87FFAEDA-9FEF-429D-9C56-F12EC1234049';
   const USER_ID = '1';
-  const THEME = 'dark';
-  const NICKNAME = 'Mich';
-  const myColorSet = {
-    '--sendbird-add-reaction-button-border-hover' : '#69c085',
-    '--sendbird-selected-reaction-button-border-hover' : '#69c085',
-    '--sendbird-dark-information-100': '#adc9ff',
 
-    '--sendbird-light-primary-500': '#adc9ff',
-    '--sendbird-light-primary-400': '#adc9ff',
-    '--sendbird-light-primary-300': '#adc9ff',
-    '--sendbird-light-primary-200': '#adc9ff',
+  const NICKNAME = 'Mich';
+
+  const myColorSet = {
+    //background color
+    '--sendbird-light-background-50':  'rgba(0, 0, 0, 0.55)',
+    //hover 
+    '--sendbird-light-background-100' : 'rgba(0, 0, 0, 0.32)',
+    //not sure -light red
+    '--sendbird-light-ondark-04': '#f66161',
+
+    //hover on my own message
+    '--sendbird-light-primary-400': '#bdbdbd',
+
+    //selected chat w/ persons name AND background color of my text?
+    '--sendbird-light-primary-300' : '#eeeeee',
+    //
+    '--sendbird-light-primary-200': 'rgba(0, 0, 0, 0.32)',
+    //selected chat
+    '--sendbird-light-primary-100'	:'rgba(0, 0, 0, 0.32)',
+    //all numbers / dates
+    '--sendbird-light-onlight-02':' #bdbdbd'
 };
 
   return (
     <div>
-      <SBProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME} allowProfileEdit={true} theme={THEME} colorSet={myColorSet} >
-        <CustomizedApp />
+      <SBProvider appId={APP_ID} 
+                  userId={USER_ID} 
+                  nickname={NICKNAME} 
+                  allowProfileEdit={true} 
+               
+                  colorSet={myColorSet} >
+        {/* customizable components inside SB Provider */}
+        <CustomizedApp /> 
       </SBProvider>
     </div>
   )
