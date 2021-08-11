ba776528f946d126576fd1e9825644b6156ae68f
michellewong27 <michellekwong2@gmail.com>
2021-08-11 11:54:28 -0400

    Updated colorSet

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -8,28 +8,38 @@ function App() {
 
   const APP_ID = '87FFAEDA-9FEF-429D-9C56-F12EC1234049';
   const USER_ID = '1';
-
-  const NICKNAME = 'Mich';
+  const NICKNAME = 'Michelle';
 
   const myColorSet = {
     //background color
-    '--sendbird-light-background-50':  'rgba(0, 0, 0, 0.55)',
-    //hover 
-    '--sendbird-light-background-100' : 'rgba(0, 0, 0, 0.32)',
-    //not sure -light red
-    '--sendbird-light-ondark-04': '#f66161',
+    '--sendbird-light-background-50':  '#474a50',
 
     //hover on my own message
-    '--sendbird-light-primary-400': '#bdbdbd',
+    '--sendbird-light-primary-400': '#33353a',
 
-    //selected chat w/ persons name AND background color of my text?
-    '--sendbird-light-primary-300' : '#eeeeee',
+    //selected chat's group name  AND background color of my text?
+    '--sendbird-light-primary-300' : '#474a50',
+   
     //
     '--sendbird-light-primary-200': 'rgba(0, 0, 0, 0.32)',
-    //selected chat
-    '--sendbird-light-primary-100'	:'rgba(0, 0, 0, 0.32)',
-    //all numbers / dates
-    '--sendbird-light-onlight-02':' #bdbdbd'
+    
+    //selected chat & EMOJI Background
+    '--sendbird-light-primary-100'	:' #55595f',
+
+    //all numbers / dates / names in chat
+    '--sendbird-light-onlight-02':' #bdbdbd',
+
+    //incoming msg font color
+   '--sendbird-light-onlight-01' : '#FFFFFF',
+    
+   //incoming msg background color 
+   '--sendbird-light-background-100' : '#474a50',
+
+   //
+  //  '--sendbird-add-reaction-button-border-hover' : '#33353a',
+    //
+  //  '--sendbird-tooltip-text-color' : '#33353a',
+
 };
 
   return (
---
+++ discord-clone/src/CustomizedMessageItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CustomizedMessageItem () {
+
+    return (
+        <div>
+            <h1>CustomizedMessageItem</h1>
+        </div>
+    )
+}
+
+export default CustomizedMessageItem;
