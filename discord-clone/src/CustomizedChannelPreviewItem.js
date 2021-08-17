f9f97611d86a2d9794bd0dfbb5e6dd73031d6a42
michellewong27 <michellekwong2@gmail.com>
2021-08-17 13:44:31 -0400

    Community Channel Lists render

--- discord-clone/src/CustomizedChannelPreviewItem.js
+++ discord-clone/src/CustomizedChannelPreviewItem.js
@@ -1,5 +1,4 @@
 import React, { useMemo, useState } from "react";
-
 import {
   Avatar,
   Badge,
--- discord-clone/src/community-components/CommunityChannelList.js
+++ discord-clone/src/community-components/CommunityChannelList.js
@@ -1,114 +1,8 @@
-// import React, { ReactElement, useEffect, useState } from 'react';
-// import withSendBird from '../../../lib/SendbirdSdkContext';
-// import sendBirdSelectors from '../../../lib/selectors';
 import React , { useEffect, useState }  from 'react';
 import { withSendBird, sendBirdSelectors } from 'sendbird-uikit';
 import './community-channel-list.scss';
 import OpenChannelPreview from './OpenChannelPreview';
 import Profile from './Profile';
-
-// interface Props {
-//   sdk: SendBird.SendBirdInstance;
-//   user: SendBird.User;
-//   currentChannelUrl: string;
-//   setCurrentChannel(channel: SendBird.OpenChannel): void;
-//   channels: Array<SendBird.OpenChannel>;
-//   setChannels(channels: Array<SendBird.OpenChannel>): void;
-// }
-
-
-// function CommunityChannelList(props){
-
-    // const [channelSource, setChannelSource] = useState<SendBird.OpenChannelListQuery>(null);
-
-    // useEffect(() => {
-    //     if (!sdk || !sdk.OpenChannel) {
-    //     return;
-    //     }
-    // })
-
-//     const openChannelListQuery = sdk.OpenChannel.createOpenChannelListQuery();
-//     // @ts-ignore: Unreachable code error
-//     openChannelListQuery.customTypes = ['SB_COMMUNITY_TYPE'];
-//     setChannelSource(openChannelListQuery);
-//     openChannelListQuery.next((openChannels, error) => {
-//       if (error) {
-//         return;
-//       }
-//       setChannels(openChannels);
-//       if (openChannels.length > 0) {
-//         setCurrentChannel(openChannels[0]);
-//       }
-//     });
-
-//   }, [sdk]);
-
-// console.log(props.channels , "channels")
-
-
-//   return (
-//     <div className="community-channel-list">
-//       <div className="community-channel-list__title">
-//         Open Channels
-//       </div>
-//       {/* <div
-//         className="community-channel-list__list"
-//         onScroll={(e: React.FormEvent<HTMLDivElement> ) => {
-//           const target = e.target as HTMLDivElement;
-//           const fetchMore = target.clientHeight + target.scrollTop === target.scrollHeight;
-//           if (fetchMore && channelSource.hasNext) {
-//             channelSource.next((openChannels, error) => {
-//               if (error) {
-//                 return;
-//               }
-//               setChannels([...channels, ...openChannels]);
-//             });
-//           }
-//         }}
-//       > */}
-//         {
-//           props.channels.length === 0
-//           ? (
-//             "No Channels"
-//           )
-//           : (
-//             <div className="community-channel-list__scroll-wrap">
-//               <div>
-//                 {
-
-// // props.channels -> returning empty array
-//                   props.channels.map(c => (
-//                     <OpenChannelPreview
-//                       key={c.url}
-//                       channel={c}
-//                       selected={c.url === props.currentChannelUrl}
-//                       onClick={
-//                         () => {props.setCurrentChannel(c)}
-//                       }
-//                     />
-//                   ))
-//                 }
-//               </div>
-//             </div>
-//           )
-//         }
-//       </div>
-
- 
-//   )
-// }
-
-
-
-// export default withSendBird(CommunityChannelList);
-
-
-
-
-
-
-
-
 
 function CommunityChannelList({
     sdk,
@@ -121,9 +15,14 @@ function CommunityChannelList({
       if (!sdk || !sdk.OpenChannel) {
         return;
       }
+
+ //be able to filter the channels in later step     
+      //return empty channels
       const openChannelListQuery = sdk.OpenChannel.createOpenChannelListQuery();
       // @ts-ignore: Unreachable code error
-      openChannelListQuery.customTypes = ["SB_COMMUNITY_TYPE"];
+
+      //filtered for specific customType 
+      // openChannelListQuery.customTypes = ["SB_COMMUNITY_TYPE"];
       openChannelListQuery.next(function (openChannels, error) {
         if (error) {
           return;
