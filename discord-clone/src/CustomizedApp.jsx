d4a341a7c6edfd3d6d97d580c2cbe1365e47eca8
michellewong27 <michellekwong2@gmail.com>
2021-08-18 12:43:09 -0400

    CSS Conversation layout

--- discord-clone/src/CustomizedApp.jsx
+++ discord-clone/src/CustomizedApp.jsx
@@ -1,5 +1,5 @@
 import "sendbird-uikit/dist/index.css";
-import React, { useState } from "react";    //useCallback
+import React, { useEffect, useState } from "react";    //useCallback
 import {   
     Channel as SBConversation,
     ChannelList as SBChannelList,
@@ -10,37 +10,35 @@ import {
 import CustomizedChannelPreviewItem from "./CustomizedChannelPreviewItem";
 import CommunityChannelList from './community-components/CommunityChannelList.jsx';
 import "./community.scss";
-// import GroupChannelConversation from "./GroupChannelConversation";
-// import OpenChannelConversation from "./OpenChannelConversation";
 
-export default function CustomizedApp({ appId, userId, nickname, accessToken,customizedPreviewItem }) {
-    
+export default function CustomizedApp({customizedPreviewItem}) {
     const [showSettings, setShowSettings] = useState(false);
     const [currentChannel, setCurrentChannel] = useState(null);
     const [channels, setChannels] = useState([""]);
     const currentChannelUrl = currentChannel ? currentChannel.url : "";
     const communityOpenChannelConversation = document.getElementsByClassName("community-open-channel__conversation-wrap");
     const groupChannelConversation = document.getElementsByClassName("group-channel__conversation-wrap");
- 
-    if(currentChannel && currentChannel.url.includes('group_channel')){
-        console.log("INCLUDES GROUP_CHANNEL")
-        communityOpenChannelConversation[0].style.display="none";
-        groupChannelConversation[0].style.display="";
-    } else if (currentChannel && currentChannel.url.includes('open_channel') ){
-        console.log("INCLUDES OPEN CHANNEL")
-        groupChannelConversation[0].style.display="none";
-        communityOpenChannelConversation[0].style.display="";
-    } else { 
-       
-    }
-
+    
+//only runs when currentChannel is changed
+    useEffect( () => {
+        if(currentChannel && currentChannel.url.includes('group_channel')){
+            console.log("INCLUDES GROUP_CHANNEL")
+            communityOpenChannelConversation[0].style.display="none";
+            groupChannelConversation[0].style.display="";
+        } else if (currentChannel && currentChannel.url.includes('open_channel') ){
+            console.log("INCLUDES OPEN CHANNEL")
+            groupChannelConversation[0].style.display="none";
+            communityOpenChannelConversation[0].style.display="";
+        }     
+    }, [currentChannel])
+   
     return (
       <div className="customized-app">
         <div className="sendbird-app__wrap">
             <div className="sendbird-app__channellist-wrap">
                 <div className="private-channel-list">
-                    {/* <div class="private-channel-list__title">Private Channels</div> */}
-                    <SBChannelList
+                    <div className="private-channel-list__title">Private Channels</div>
+                        <SBChannelList
                             onChannelSelect={(channel) => {
                                 if (channel && channel.url) {
                                     //setCurrentChannelUrl(channel.url);
@@ -51,7 +49,6 @@ export default function CustomizedApp({ appId, userId, nickname, accessToken,cus
                                 customizedPreviewItem
                                 ? ({ channel, onLeaveChannel }) => (
                                     <CustomizedChannelPreviewItem
-                                        // userId={userId}
                                         channel={channel}
                                         onLeaveChannel={onLeaveChannel}
                                         currentChannelUrl={currentChannelUrl}
@@ -66,13 +63,12 @@ export default function CustomizedApp({ appId, userId, nickname, accessToken,cus
                         <CommunityChannelList
                             currentChannelUrl={currentChannelUrl}
                             setCurrentChannel={setCurrentChannel}
-                            userId={userId}
                         /> 
                     </div>
                 </div>
             </div>
-            <div className="sendbird-app__conversation-wrap"  >
-                <div className="community-open-channel__conversation-wrap"  >
+            <div className="sendbird-app__conversation-wrap">
+                <div className="community-open-channel__conversation-wrap" >
                     <OpenChannel
                         channelUrl={currentChannelUrl}
                         onChatHeaderActionClick={() => {
